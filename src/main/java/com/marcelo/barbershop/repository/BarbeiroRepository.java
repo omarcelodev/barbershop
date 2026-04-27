d998540d9f2598e8ae2fd83ff883b8e2d9694fd4
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-04-27 10:05:18 -0300

    feat(BarbeiroRepository.java): Add findBuUsuario | findByUsuarioId | existsByUsuario | findAllByAtivo | findAtivosComServico

--- src/main/java/com/marcelo/barbershop/repository/BarbeiroRepository.java
+++ src/main/java/com/marcelo/barbershop/repository/BarbeiroRepository.java
@@ -1,5 +1,24 @@
 package com.marcelo.barbershop.repository;
 
-public class BarbeiroRepository {
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import com.marcelo.barbershop.entity.Barbeiro;
+import com.marcelo.barbershop.entity.Usuario;
+
+public interface BarbeiroRepository extends JpaRepository<Barbeiro, Long>{
+
+    Optional<Barbeiro> findByUsuario(Usuario usuario);
+
+    Optional<Barbeiro> findByUsuarioId(Long usuarioId);
+
+    boolean existsByUsuario(Usuario usuario);
+
+    List<Barbeiro> findAllByAtivo(Boolean ativo);
+
+    @Query("SELECT b FROM Barbeiro b JOIN b.servicos s WHERE s.id = :servicoId AND b.ativo = true")
+    List<Barbeiro> findAtivosComServico(Long servicoId);
+
     
 }
