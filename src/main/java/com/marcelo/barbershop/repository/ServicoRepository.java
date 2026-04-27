b1288125b9597122d54aaca9e9b99d657c433dd3
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-04-27 09:40:20 -0300

    feat(ServicoRepository.java): Add findByNome() and existisByNome()

--- src/main/java/com/marcelo/barbershop/repository/ServicoRepository.java
+++ src/main/java/com/marcelo/barbershop/repository/ServicoRepository.java
@@ -1,5 +1,15 @@
 package com.marcelo.barbershop.repository;
 
-public class ServicoRepository {
+import com.marcelo.barbershop.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+
+@Repository
+public interface ServicoRepository extends JpaRepository<Servico, Long>{
+    Optional<Servico> findByNome(String nome);
+
+    boolean existsByNome(String nome);
     
 }
