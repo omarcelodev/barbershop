b7bc03d946c21010b4262db1efca64a9ffcb2a73
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-04-27 09:54:22 -0300

    feat(UsuarioRepository.java): Add findByEmail() | existsByEmail | findAllByAtivo | findAllByRole

--- src/main/java/com/marcelo/barbershop/repository/UsuarioRepository.java
+++ src/main/java/com/marcelo/barbershop/repository/UsuarioRepository.java
@@ -1,5 +1,21 @@
 package com.marcelo.barbershop.repository;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.marcelo.barbershop.entity.Usuario;
+import com.marcelo.barbershop.entity.Role;
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existByEmail(String email);
+
+    List<Usuario> findAllByAtivo(Boolean ativo);
+
+    List<Usuario> findAllByRole(Role role);
     
 }
