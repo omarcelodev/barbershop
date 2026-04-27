430cc56217cb61fe58e3d32dc20ddb27de33349a
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-04-27 11:19:33 -0300

    feat(NotificacaoRepository.java): Add findALLByAgendamentoId() | findAllByStatus() | findAllByStatusOrderByCriadoEmAsc

--- src/main/java/com/marcelo/barbershop/repository/NotificacaoRepository.java
+++ src/main/java/com/marcelo/barbershop/repository/NotificacaoRepository.java
@@ -1,5 +1,18 @@
 package com.marcelo.barbershop.repository;
 
-public class NotificacaoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcelo.barbershop.entity.Notificacao;
+import com.marcelo.barbershop.entity.StatusNotificacao;
+
+public interface NotificacaoRepository extends JpaRepository<Notificacao, Long> {
+
+    List<Notificacao> findALLByAgendamentoId(Long agendamentoId);
+
+    List<Notificacao> findAllByStatus(StatusNotificacao status);
+
+    List<Notificacao> findAllByStatusOrderByCriadoEmAsc(StatusNotificacao status);
     
 }
